056f31839fefdce9cd2733e001039175822b7ba4
Jan Gosmann <jgosmann@uwaterloo.ca>
2018-12-16 19:33:01 +0100

    Auto-generate fields for Nengo objects.

--- nengonized_kernel/gql_schema.py
+++ nengonized_kernel/gql_schema.py
@@ -1,28 +1,58 @@
-from graphene import Field, List, NonNull, ObjectType, Schema, String
+import warnings
+
+import graphene as g
+import nengo
+import nengo.params as p
+from nengo.params import iter_params
 
 
-class NengoObject(ObjectType):
-    label = String()
+class GqlFieldsFromParams(g.ObjectType):
+    paramTypeToGqlType = {
+        p.StringParam: g.String,
+        p.BoolParam: g.Boolean,
+        p.NumberParam: g.Float,
+        p.IntParam: g.Int,
+    }
 
     def __init__(self, obj):
-        super()
         self._obj = obj
 
-    def resolve_label(self, info):
-        return self._obj.label
+    def __init_subclass__(cls, backing_class, **kwargs):
+        for param_name in iter_params(backing_class):
+            param = getattr(backing_class, param_name)
+            if (param.name not in vars(cls)
+                    and type(param) in cls.paramTypeToGqlType):
+                cls.bind_param(param)
+            else:
+                warnings.warn(
+                    f"Cannot bind param '{cls.__name__}.{param.name}' of type "
+                    f"'{type(param).__name__}' because corresponding Graphene "
+                    f"type is unknown.")
+        super().__init_subclass__(**kwargs)
+
+    @classmethod
+    def bind_param(cls, param):
+        field_type = cls.paramTypeToGqlType[type(param)]
+        if not param.optional:
+            field_type = g.NonNull(field_type)
+        else:
+            field_type = field_type()
+        setattr(cls, param.name, field_type)
+        setattr(cls, 'resolve_' + param.name,
+                lambda self, info, name=param.name: getattr(self._obj, name))
 
 
-class Ensemble(NengoObject):
+class Ensemble(GqlFieldsFromParams, backing_class=nengo.Ensemble):
     pass
 
 
-class Node(NengoObject):
+class Node(GqlFieldsFromParams, backing_class=nengo.Node):
     pass
 
 
-class Network(ObjectType):
-    ensembles = NonNull(List(NonNull(Ensemble)))
-    nodes = NonNull(List(NonNull(Node)))
+class Network(g.ObjectType):
+    ensembles = g.NonNull(g.List(g.NonNull(Ensemble)))
+    nodes = g.NonNull(g.List(g.NonNull(Node)))
 
     def __init__(self, net):
         super()
@@ -35,11 +65,11 @@ class Network(ObjectType):
         return [Node(node) for node in self._net.nodes]
 
 
-class RootQuery(ObjectType):
-    model = Field(Network)
+class RootQuery(g.ObjectType):
+    model = g.Field(Network)
 
     def resolve_model(self, info):
         return Network(info.context)
 
 
-nengo_model_schema = Schema(query=RootQuery)
+nengo_model_schema = g.Schema(query=RootQuery)
