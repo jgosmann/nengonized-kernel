98fc6357851af8741e047885b4c40b56f495ee80
Jan Gosmann <jgosmann@uwaterloo.ca>
2018-12-16 21:40:32 +0100

    Presever working directory

--- nengonized_kernel/model_loader.py
+++ nengonized_kernel/model_loader.py
@@ -1,5 +1,19 @@
+from contextlib import contextmanager
+import os
+
+
+@contextmanager
+def preserve_cwd():
+    saved_cwd = os.getcwd()
+    try:
+        yield
+    finally:
+        os.chdir(saved_cwd)
+
+
 class ModelLoader(object):
     def from_string(self, code):
         locals_dict = {}
-        exec(code, locals_dict)
+        with preserve_cwd():
+            exec(code, locals_dict)
         return locals_dict['model']
--- nengonized_kernel/tests/test_model_loader.py
+++ nengonized_kernel/tests/test_model_loader.py
@@ -1,3 +1,5 @@
+import os
+
 import nengo
 
 from nengonized_kernel.model_loader import ModelLoader
@@ -10,8 +12,15 @@ class TestModelLoader(object):
         assert isinstance(model, nengo.Network)
         assert model.label == 'dummy'
 
+    def test_does_not_change_working_directory(self):
+        code = "import os; os.chdir('..')\n" + dummy_model
+        expected = os.getcwd()
+        ModelLoader().from_string(code)
+        actual = os.getcwd()
+        os.chdir(expected)  # prevent side-effects on other tests
+        assert actual == expected
+
     # TODO
-    # workdir test!
     # error cases
     # __nengo_gui__ var/__file__
     # no model
