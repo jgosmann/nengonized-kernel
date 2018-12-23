7ed4d7cc4763a8bae471e14659c94c7e3afa0448
Jan Gosmann <jgosmann@uwaterloo.ca>
2018-12-23 14:35:14 +0100

    Add IdProvider.

---
+++ nengonized_kernel/id_provider.py
@@ -0,0 +1,69 @@
+import weakref
+
+import nengo
+
+
+class IdProvider(object):
+    valid_types = {
+        nengo.Network: 'Network',
+        nengo.Ensemble: 'Ensemble',
+        nengo.Node: 'Node',
+    }
+
+    def __init__(self, model, locals_dict=None, prefix='no'):
+        self.prefix = prefix
+        self._unique_name_map = weakref.WeakKeyDictionary()
+        self._type_map = weakref.WeakKeyDictionary()
+        if locals_dict:
+            self._scan_locals(locals_dict)
+        self._scan_networks(model)
+        self._scan_standard_lists(model)
+
+    def _scan_locals(self, locals_dict):
+        for k, v in locals_dict.items():
+            self._process_candidate(v, k)
+
+    def _scan_networks(self, net):
+        to_process = [net]
+        while len(to_process) > 0:
+            net = to_process.pop()
+            for attr_name in dir(net):
+                value = getattr(net, attr_name)
+                self._process_candidate(
+                        value, f'{self._unique_name_map[net]}.{attr_name}')
+                if isinstance(value, nengo.Network):
+                    to_process.append(value)
+
+    def _scan_standard_lists(self, net):
+        to_process = [net]
+        while len(to_process) > 0:
+            net = to_process.pop()
+            for i, subnet in enumerate(net.networks):
+                self._process_candidate(
+                    subnet, f'{self._unique_name_map[net]}.networks[{i}]')
+                to_process.append(subnet)
+            for i, ens in enumerate(net.ensembles):
+                self._process_candidate(
+                    ens, f'{self._unique_name_map[net]}.ensembles[{i}]')
+            for i, node in enumerate(net.nodes):
+                self._process_candidate(
+                    node, f'{self._unique_name_map[net]}.nodes[{i}]')
+
+    def _process_candidate(self, obj, unique_name):
+        if obj in self:
+            return
+
+        for base in type(obj).__mro__:
+            if base in self.valid_types:
+                self._unique_name_map[obj] = unique_name
+                self._type_map[obj] = self.valid_types[base]
+                return
+
+    def _get_id(self, type_name, unique_name):
+        return ':'.join((self.prefix, type_name, unique_name))
+
+    def __contains__(self, key):
+        return key in self._type_map and key in self._unique_name_map
+
+    def __getitem__(self, key):
+        return self._get_id(self._type_map[key], self._unique_name_map[key])
---
+++ nengonized_kernel/tests/test_id_provider.py
@@ -0,0 +1,62 @@
+import gc
+
+import nengo
+import pytest
+import weakref
+
+from nengonized_kernel.id_provider import IdProvider
+
+
+def test_provides_id_for_nengo_objects_based_on_locals_dict():
+    with nengo.Network() as model:
+        with nengo.Network() as subnetwork:
+            node = nengo.Node(0.)
+        ens = nengo.Ensemble(10, 1)
+
+    id_provider = IdProvider(model, locals(), prefix='no')
+
+    assert id_provider[model] == 'no:Network:model'
+    assert id_provider[subnetwork] == 'no:Network:subnetwork'
+    assert id_provider[node] == 'no:Node:node'
+    assert id_provider[ens] == 'no:Ensemble:ens'
+
+
+def test_does_not_keep_strong_references():
+    with nengo.Network() as model:
+        ens = nengo.Ensemble(10, 1)
+
+    model_ref = weakref.ref(model)
+    ens_ref = weakref.ref(ens)
+
+    id_provider = IdProvider(model, {'model': model, 'ens': ens})
+    del model, ens
+
+    assert model_ref() is None
+    assert ens_ref() is None
+
+
+def test_provides_id_for_network_attributes():
+    with nengo.Network() as model:
+        model.ens = nengo.Ensemble(10, 1)
+        with nengo.Network() as model.subnetwork:
+            model.subnetwork.ens = nengo.Ensemble(10, 1)
+
+    id_provider = IdProvider(model, {'model': model}, prefix='no')
+
+    assert id_provider[model.ens] == 'no:Ensemble:model.ens'
+    assert id_provider[model.subnetwork] == 'no:Network:model.subnetwork'
+    assert id_provider[model.subnetwork.ens] == (
+            'no:Ensemble:model.subnetwork.ens')
+
+
+def test_provides_id_from_standard_lists():
+    with nengo.Network() as model:
+        ens = nengo.Ensemble(10, 1)
+        node = nengo.Node(0.)
+        net = nengo.Network()
+
+    id_provider = IdProvider(model, {'model': model}, prefix='no')
+
+    assert id_provider[ens] == 'no:Ensemble:model.ensembles[0]'
+    assert id_provider[node] == 'no:Node:model.nodes[0]'
+    assert id_provider[net] == 'no:Network:model.networks[0]'
